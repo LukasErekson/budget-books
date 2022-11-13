74488ade749cecbb1e772d6890d574276ba4119e
Lukas <Lukas.Erekson@gmail.com>
2022-11-13 10:50:00 -0700

    Create BadResponseError error class
    - Create a custom error class to use with DataFetch
      errors so that the specific error message can be
      captured from the server.

---
+++ budget-books-frontend/src/Common/BadResponseError.tsx
@@ -0,0 +1,15 @@
+class BadResponseError extends Error {
+    status: number;
+    serverError: string;
+
+    constructor(status: number, message: string, serverError: string) {
+        super(message);
+
+        this.status = status;
+        this.serverError = serverError;
+
+        Object.setPrototypeOf(this, BadResponseError.prototype);
+    }
+}
+
+export default BadResponseError;
