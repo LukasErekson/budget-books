f97e9daf6cfe5f8655104665016964d28073a1f1
Lukas <Lukas.Erekson@gmail.com>
2023-01-05 15:08:20 -0700

    Fix a bug with the date-formatting functions
    - Fix bug where dates less than 10 wouldn't have the
      prepending 0.

--- budget-books-frontend/src/Common/TextFilters.tsx
+++ budget-books-frontend/src/Common/TextFilters.tsx
@@ -7,11 +7,19 @@ function pyToJsDate(pyDate: string): string {
 }
 
 function dayMonthYear(jsDate: Date): string {
-  return `${jsDate.getDate()}-${jsDate.getMonth() + 1}-${jsDate.getFullYear()}`;
+  let numericMonth: number = jsDate.getMonth() + 1;
+  let numericDay: number = jsDate.getDate();
+  return `${(numericDay < 10 ? '0' : '') + numericDay.toString()}-${
+    (numericMonth < 10 ? '0' : '') + numericMonth.toString()
+  }-${jsDate.getFullYear()}`;
 }
 
 function yearMonthDay(jsDate: Date): string {
-  return `${jsDate.getFullYear()}-${jsDate.getMonth() + 1}-${jsDate.getDate()}`;
+  let numericMonth: number = jsDate.getMonth() + 1;
+  let numericDay: number = jsDate.getDate();
+  return `${jsDate.getFullYear()}-${
+    (numericMonth < 10 ? '0' : '') + numericMonth.toString()
+  }-${(numericDay < 10 ? '0' : '') + numericDay.toString()}`;
 }
 
 export { pyToJsDate, dayMonthYear, yearMonthDay };
