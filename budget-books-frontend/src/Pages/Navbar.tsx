26a5038c83ff79f8f8c56a89db3b38572dbef6ae
Lukas <Lukas.Erekson@gmail.com>
2023-01-08 16:42:47 -0700

    [Issue #12] Begin building Nav bar
    - Create the basic structure in the Navbar component.
    - Start styling the nav bar with simple styling.

--- budget-books-frontend/src/Pages/Navbar.tsx
+++ budget-books-frontend/src/Pages/Navbar.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { GiWhiteBook } from 'react-icons/gi';
 
 function Navbar(): JSX.Element {
   return (
-    <>
-      <li>Links</li>
-    </>
+    <nav>
+      <ul className='nav-list'>
+        <li className='nav-list-item'>
+          <a href='/'>
+            <GiWhiteBook style={{ width: '1.5rem', height: '1.5rem' }} /> Budget
+            Books
+          </a>
+        </li>
+        <li className='nav-list-item'>
+          <a href='/'>Categorize Transactions</a>
+        </li>
+        <li className='nav-list-item'>
+          <a href='/'>Balance Sheet</a>
+        </li>
+        <li className='nav-list-item'>
+          <a href='/'>Expense Report</a>
+        </li>
+        <li className='nav-list-item'>
+          <a href='/'>Account Settings</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
