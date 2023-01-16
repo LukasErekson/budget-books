8b51a11f0d1b636ce6316a5c3dda50ef83f80997
Lukas <Lukas.Erekson@gmail.com>
2023-01-16 13:11:51 -0700

    Migrate index.js to TypeScript
    - Install the React-Router-DOM and migrate the
      index.js file to its Typescript coutnerpart.

---
+++ budget-books-frontend/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './index.css';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
