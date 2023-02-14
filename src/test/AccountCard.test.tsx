142d564ecf6e4479e8b13d6af4ca3ef4eed5de96
Lukas <Lukas.Erekson@gmail.com>
2023-02-14 07:27:05 -0700

    [Issue #15] Rewrite the redux store for testing
    - Refactor the Redux store code to allow for ease of testing
      without changing the behavior of the app itself.

--- src/test/AccountCard.test.tsx
+++ src/test/AccountCard.test.tsx
@@ -1,9 +1,11 @@
-import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { AccountCard } from '../features/Accounts';
 import Account from '../features/Accounts/types/types';
-import store from '../stores/store';
 import * as transactionSelectors from '../features/Transactions/stores/transactionSelectors';
+import { setTransactions } from '../features/Transactions/stores/transactionSlice';
+import { setupStore } from '../stores/store';
+import { renderWithProviders } from './setupTests';
+
 const testAccount: Account = {
   id: 1,
   name: 'Testing Account',
@@ -16,28 +18,22 @@ const testAccount: Account = {
 
 describe('AccountCard Component', () => {
   it('Renders the Account Card without Error', async () => {
-    render(
-      <Provider store={store}>
-        <AccountCard accountData={testAccount} onClick={() => {}} />
-      </Provider>
+    renderWithProviders(
+      <AccountCard accountData={testAccount} onClick={() => {}} />
     );
   });
 
   it('Displays the account name', async () => {
-    render(
-      <Provider store={store}>
-        <AccountCard accountData={testAccount} onClick={() => {}} />
-      </Provider>
+    renderWithProviders(
+      <AccountCard accountData={testAccount} onClick={() => {}} />
     );
 
     expect(await screen.findByText(testAccount.name)).toBeDefined();
   });
 
   it('Displays the account type group', async () => {
-    render(
-      <Provider store={store}>
-        <AccountCard accountData={testAccount} onClick={() => {}} />
-      </Provider>
+    renderWithProviders(
+      <AccountCard accountData={testAccount} onClick={() => {}} />
     );
 
     expect(await screen.findByText(testAccount.account_type)).toBeDefined();
@@ -45,20 +41,16 @@ describe('AccountCard Component', () => {
 
   describe('Balance Display', () => {
     it('Rounds dispalyed balance to 2 decimal places', async () => {
-      render(
-        <Provider store={store}>
-          <AccountCard accountData={testAccount} onClick={() => {}} />
-        </Provider>
+      renderWithProviders(
+        <AccountCard accountData={testAccount} onClick={() => {}} />
       );
 
       expect(await screen.findByText('-$10.00')).toBeDefined();
     });
 
     it('Adds negative class to negative balances', async () => {
-      render(
-        <Provider store={store}>
-          <AccountCard accountData={testAccount} onClick={() => {}} />
-        </Provider>
+      renderWithProviders(
+        <AccountCard accountData={testAccount} onClick={() => {}} />
       );
 
       let balance = await screen.findByText('-$10.00');
@@ -67,13 +59,11 @@ describe('AccountCard Component', () => {
     });
 
     it('Adds positive class to positive balances', async () => {
-      render(
-        <Provider store={store}>
-          <AccountCard
-            accountData={{ ...testAccount, balance: 10.0 }}
-            onClick={() => {}}
-          />
-        </Provider>
+      renderWithProviders(
+        <AccountCard
+          accountData={{ ...testAccount, balance: 10.0 }}
+          onClick={() => {}}
+        />
       );
 
       let balance = await screen.findByText('$10.00');
@@ -84,13 +74,11 @@ describe('AccountCard Component', () => {
 
   describe('Uncategorized display', () => {
     it('Displays a muted 0 if there are no uncategorized transactions', async () => {
-      render(
-        <Provider store={store}>
-          <AccountCard
-            accountData={{ ...testAccount, balance: 10.0 }}
-            onClick={() => {}}
-          />
-        </Provider>
+      renderWithProviders(
+        <AccountCard
+          accountData={{ ...testAccount, balance: 10.0 }}
+          onClick={() => {}}
+        />
       );
 
       let uncategorizedTransactions = await screen.findByText('0');
@@ -103,31 +91,33 @@ describe('AccountCard Component', () => {
     });
 
     it('Displays an alerted 1 if there is one uncategorized transaction', async () => {
-      let uncategorizedSpy = jest.spyOn(
-        transactionSelectors,
-        'selectUncategorizedTransactions'
+      const testStore = setupStore();
+
+      const dispatch = testStore.dispatch;
+
+      dispatch(
+        setTransactions({
+          transactions: [
+            {
+              id: 1,
+              name: 'Test Transcation',
+              description: 'This transaction is only a test',
+              amount: -10.99,
+              debit_account_id: 1,
+              credit_account_id: 'undefined',
+              transaction_date: '2022-02-22',
+              date_entered: '2022-02-22',
+            },
+          ],
+        })
       );
 
-      uncategorizedSpy.mockReturnValue([
-        {
-          id: 1,
-          name: 'Test Transcation',
-          description: 'This transaction is only a test',
-          amount: -10.99,
-          debit_account_id: 1,
-          credit_account_id: 0,
-          transaction_date: '2022-02-22',
-          date_entered: '2022-02-22',
-        },
-      ]);
-
-      render(
-        <Provider store={store}>
-          <AccountCard
-            accountData={{ ...testAccount, balance: 10.0 }}
-            onClick={() => {}}
-          />
-        </Provider>
+      renderWithProviders(
+        <AccountCard
+          accountData={{ ...testAccount, balance: 10.0 }}
+          onClick={() => {}}
+        />,
+        { store: testStore }
       );
 
       let uncategorizedTransactions = await screen.findByText('1');
