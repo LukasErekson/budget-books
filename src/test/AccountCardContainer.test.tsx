9d1481fc9d6dc3a83d584f410924c8c500c82b25
Lukas <Lukas.Erekson@gmail.com>
2023-02-15 20:13:07 -0700

    [Issue #15] Refactor AcctCardContainer tests
    - Refactor some of the mocks and test stores used in
      the AccountCardContainer tests to reduce reused code
      and improve readability.
    - Write an additional test for updating the active account
      in the store.

--- src/test/AccountCardContainer.test.tsx
+++ src/test/AccountCardContainer.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
 import Account from '../features/Accounts/types/types';
 import { AccountCardContainer } from '../features/Accounts';
@@ -6,88 +6,27 @@ import * as AccountThunks from '../features/Accounts/stores/accountThunks';
 import * as AccountTypeThunks from '../features/AccountTypes/stores/accountTypeThunks';
 import * as TransactionThunks from '../features/Transactions/stores/transactionThunks';
 
-import { setupStore } from '../stores/store';
+import { RootState, setupStore } from '../stores/store';
 import { renderWithProviders } from './setupTests';
 import * as DataFetch from '../utils/DataFetch';
 import AccountType from '../features/AccountTypes/types/types';
 
 describe('AccountCardContainer Component', () => {
   let DataFetchMock = jest.spyOn(DataFetch, 'default');
+  let testStore: RootState;
 
-  beforeEach(() => {
+  const fakePromise: Promise<any> = new Promise(() => {});
+
+  beforeAll(() => {
     DataFetchMock.mockImplementation(
       (method: string, url: RequestInfo, requestData?: any) => {
         return {
           cancel: () => {},
-          responsePromise: new Promise(() => {}),
+          responsePromise: fakePromise,
         };
       }
     );
-  });
 
-  afterEach(() => {
-    DataFetchMock.mockReset();
-  });
-
-  it('Renders without error', async () => {
-    renderWithProviders(<AccountCardContainer />);
-
-    expect(DataFetchMock).toHaveBeenCalledTimes(2);
-
-    expect(DataFetchMock).toHaveReturnedTimes(2);
-
-    expect(await screen.findByText(/loading accounts.../i)).toBeDefined();
-  });
-
-  it('Displays account cards with loaded accounts', async () => {
-    let fakeAccounts: Account[] = [
-      {
-        id: 1,
-        name: 'Fake Account 1',
-        account_type_id: 1,
-        account_type: 'Checking Account',
-        debit_inc: true,
-        balance: 10.27,
-        last_updated: '2022-02-22',
-      },
-      {
-        id: 1,
-        name: 'Fake Account 2',
-        account_type_id: 2,
-        account_type: 'Credit Card',
-        debit_inc: false,
-        balance: -10.27,
-        last_updated: '2022-02-22',
-      },
-    ];
-    const testStore = setupStore({ accounts: { accounts: fakeAccounts } });
-
-    let fetchAccountsMock = jest.spyOn(AccountThunks, 'fetchAccounts');
-    fetchAccountsMock.mockReturnValue(() => new Promise(() => {}));
-
-    let fetchAccountTypesMock = jest.spyOn(
-      AccountTypeThunks,
-      'fetchAccountTypes'
-    );
-    fetchAccountTypesMock.mockReturnValue(() => new Promise(() => {}));
-
-    let fetchBankAccountTransactionsMock = jest.spyOn(
-      TransactionThunks,
-      'fetchBankAccountTransactions'
-    );
-    fetchBankAccountTransactionsMock.mockReturnValue(
-      () => new Promise(() => {})
-    );
-
-    renderWithProviders(<AccountCardContainer />, { store: testStore });
-
-    expect(fetchAccountsMock).toHaveBeenCalledTimes(1);
-    expect(fetchAccountTypesMock).toHaveBeenCalledTimes(1);
-
-    expect(await screen.findByText('Fake Account 1')).toBeDefined();
-  });
-
-  it('Opens the new account modal on click', async () => {
     let fakeAccounts: Account[] = [
       {
         id: 1,
@@ -121,30 +60,91 @@ describe('AccountCardContainer Component', () => {
         group: 'Liabilities',
       },
     ];
-    const testStore = setupStore({
+    testStore = setupStore({
       accounts: { accounts: fakeAccounts },
       accountTypes: {
         accountTypes: fakeAccountTypes,
         accountGroups: fakeAccountTypes.map((accountType) => accountType.group),
       },
     });
+  });
 
+  afterEach(() => {
+    DataFetchMock.mockClear();
+  });
+
+  it('Renders without error', async () => {
+    renderWithProviders(<AccountCardContainer />);
+
+    expect(DataFetchMock).toHaveBeenCalledTimes(2);
+
+    expect(DataFetchMock).toHaveReturnedTimes(2);
+
+    expect(await screen.findByText(/loading accounts.../i)).toBeDefined();
+  });
+
+  it('Displays account cards with loaded accounts', async () => {
     let fetchAccountsMock = jest.spyOn(AccountThunks, 'fetchAccounts');
-    fetchAccountsMock.mockReturnValue(() => new Promise(() => {}));
+    fetchAccountsMock.mockReturnValue(() => fakePromise);
 
     let fetchAccountTypesMock = jest.spyOn(
       AccountTypeThunks,
       'fetchAccountTypes'
     );
-    fetchAccountTypesMock.mockReturnValue(() => new Promise(() => {}));
+    fetchAccountTypesMock.mockReturnValue(() => fakePromise);
 
     let fetchBankAccountTransactionsMock = jest.spyOn(
       TransactionThunks,
       'fetchBankAccountTransactions'
     );
-    fetchBankAccountTransactionsMock.mockReturnValue(
-      () => new Promise(() => {})
+    fetchBankAccountTransactionsMock.mockReturnValue(() => fakePromise);
+
+    renderWithProviders(<AccountCardContainer />, { store: testStore });
+
+    expect(fetchAccountsMock).toHaveBeenCalledTimes(1);
+    expect(fetchAccountTypesMock).toHaveBeenCalledTimes(1);
+
+    expect(await screen.findByText('Fake Account 1')).toBeDefined();
+  });
+
+  it('Updates the active account on clicking an account card', async () => {
+    let state: RootState = testStore.getState();
+
+    renderWithProviders(<AccountCardContainer />, { store: testStore });
+
+    expect(state.pageSlice.categorizationPage.activeAccount).toEqual({});
+
+    let firstAccount = await screen.findByText('Fake Account 1');
+    fireEvent.click(firstAccount);
+
+    state = testStore.getState();
+
+    expect(state.pageSlice.categorizationPage.activeAccount).toEqual({
+      id: 1,
+      name: 'Fake Account 1',
+      account_type_id: 1,
+      account_type: 'Checking Account',
+      debit_inc: true,
+      balance: 10.27,
+      last_updated: '2022-02-22',
+    });
+  });
+
+  it('Opens the new account modal on click', async () => {
+    let fetchAccountsMock = jest.spyOn(AccountThunks, 'fetchAccounts');
+    fetchAccountsMock.mockReturnValue(() => fakePromise);
+
+    let fetchAccountTypesMock = jest.spyOn(
+      AccountTypeThunks,
+      'fetchAccountTypes'
     );
+    fetchAccountTypesMock.mockReturnValue(() => fakePromise);
+
+    let fetchBankAccountTransactionsMock = jest.spyOn(
+      TransactionThunks,
+      'fetchBankAccountTransactions'
+    );
+    fetchBankAccountTransactionsMock.mockReturnValue(() => fakePromise);
 
     renderWithProviders(<AccountCardContainer />, { store: testStore });
 
