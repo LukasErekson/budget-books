44caf7a9378efc074114e9daa46794c6f9adee61
Lukas <Lukas.Erekson@gmail.com>
2023-04-25 10:25:56 -0600

    [Issue #15] Begin writing Transaction Thunk Tests

--- src/test/Transactions/stores/transactionThunks.test.ts
+++ src/test/Transactions/stores/transactionThunks.test.ts
@@ -13,42 +13,298 @@ import { setupStore } from '../../../stores/store';
 import * as TransactionActions from '../../../features/Transactions/stores/transactionSlice';
 import * as DataFetch from '../../../utils/DataFetch';
 
-import { mockThunkReturn } from '../../setupTests';
 import Transaction from '../../../features/Transactions/types/types';
-import BadResponseError from '../../../utils/BadResponseError';
+import * as BadResponseError from '../../../utils/BadResponseError';
+import Account from '../../../features/Accounts/types/types';
 
 describe('Transaction Thunks', () => {
-  describe('fetchAccountTransactions', () => {
-    describe('Given an ok response', () => {
-      it.todo('Defaults categorized_status to "all"');
+  const DataFetchMock = jest.spyOn(DataFetch, 'default');
+  const BadResponseMock = jest.spyOn(BadResponseError, 'default');
 
-      it.todo('Dispatches setTransactions and setTransactionsIsLoaded');
+  const mockStore = setupStore();
+  const dispatch = mockStore.dispatch;
+
+  const fakeTransactions: Transaction[] = [
+    {
+      id: 1,
+      name: 'Jesting Supplies',
+      description: 'Jesting supplies for all your testing needs!',
+      amount: 30.25,
+      debit_account_id: 1,
+      credit_account_id: 'undefined',
+      transaction_date: '03/20/2023',
+      date_entered: '04/13/2023',
+    },
+    {
+      id: 3,
+      name: 'Credit card payment',
+      description: 'Credit Card Payment',
+      amount: 30.25,
+      debit_account_id: 'undefined',
+      credit_account_id: 1,
+      transaction_date: '03/22/2023',
+      date_entered: '04/13/2023',
+    },
+  ];
+
+  const fakeAccounts: Account[] = [
+    {
+      id: 1,
+      name: 'Account 1',
+      account_type_id: 1,
+      account_type: 'Test',
+      debit_inc: true,
+      balance: 0.0,
+      last_updated: '01/01/2023',
+    },
+    {
+      id: 2,
+      name: 'Account 22',
+      account_type_id: 1,
+      account_type: 'Test',
+      debit_inc: true,
+      balance: 0.0,
+      last_updated: '01/01/2023',
+    },
+  ];
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('fetchAccountTransactions', () => {
+    it('Defaults categorized_status to "all"', async () => {
+      const dataFetchReturn = {
+        responsePromise: new Promise<any>((resolve, reject) => {
+          resolve({
+            ok: true,
+            json: () => ({
+              transactions: fakeTransactions,
+            }),
+          });
+        }),
+        cancel: () => null,
+      };
+
+      DataFetchMock.mockReturnValue(dataFetchReturn);
+
+      await dispatch(fetchAccountTransactions(fakeAccounts[0]));
+
+      expect(DataFetchMock).toHaveBeenCalledWith(
+        'GET',
+        '/api/transactions?account_ids=1&categorize_type=all'
+      );
+    });
+
+    describe('Given an ok response', () => {
+      it('Dispatches setTransactions and setTransactionsIsLoaded', async () => {
+        const dataFetchReturn = {
+          responsePromise: new Promise<any>((resolve, reject) => {
+            resolve({
+              ok: true,
+              json: () => ({
+                transactions: fakeTransactions,
+              }),
+            });
+          }),
+          cancel: () => null,
+        };
+
+        DataFetchMock.mockReturnValue(dataFetchReturn);
+
+        const setTransactions = jest.spyOn(
+          TransactionActions,
+          'setTransactions'
+        );
+
+        const setTransactionsIsLoaded = jest.spyOn(
+          TransactionActions,
+          'setTransactionsIsLoaded'
+        );
+
+        await dispatch(fetchAccountTransactions(fakeAccounts[0]));
+
+        expect(setTransactions).toHaveBeenCalledWith({
+          transactions: fakeTransactions,
+        });
+
+        expect(setTransactionsIsLoaded).toHaveBeenCalledWith({ loaded: true });
+      });
     });
 
     describe('Given a bad response', () => {
-      it.todo('Throws a BadResponseError');
+      it('Throws a BadResponseError', async () => {
+        const dataFetchReturn = {
+          responsePromise: new Promise<any>((resolve, reject) => {
+            resolve({
+              ok: false,
+              json: () => ({
+                transactions: fakeTransactions,
+              }),
+              status: 500,
+            });
+          }),
+          cancel: () => null,
+        };
+
+        DataFetchMock.mockReturnValue(dataFetchReturn);
+
+        await dispatch(fetchAccountTransactions(fakeAccounts[0]));
+
+        expect(BadResponseMock).toHaveBeenCalled();
+      });
     });
   });
-  describe('fetchBankAccountTransactions', () => {
-    describe('Given an ok response', () => {
-      it.todo('Defaults categorized_status to "all"');
 
-      it.todo('Dispatches setTransactions and setTransactionsIsLoaded');
+  describe('fetchBankAccountTransactions', () => {
+    it('Defaults categorized_status to "all"', async () => {
+      const dataFetchReturn = {
+        responsePromise: new Promise<any>((resolve, reject) => {
+          resolve({
+            ok: true,
+            json: () => ({
+              transactions: fakeTransactions,
+            }),
+          });
+        }),
+        cancel: () => null,
+      };
+
+      DataFetchMock.mockReturnValue(dataFetchReturn);
+
+      await dispatch(fetchBankAccountTransactions(fakeAccounts));
+
+      expect(DataFetchMock).toHaveBeenCalledWith(
+        'GET',
+        '/api/transactions?account_ids=1,2&categorize_type=all'
+      );
+    });
+
+    describe('Given an ok response', () => {
+      it('Dispatches setTransactions and setTransactionsIsLoaded', async () => {
+        const dataFetchReturn = {
+          responsePromise: new Promise<any>((resolve, reject) => {
+            resolve({
+              ok: true,
+              json: () => ({
+                transactions: fakeTransactions,
+              }),
+            });
+          }),
+          cancel: () => null,
+        };
+
+        DataFetchMock.mockReturnValue(dataFetchReturn);
+
+        const setTransactions = jest.spyOn(
+          TransactionActions,
+          'setTransactions'
+        );
+
+        const setTransactionsIsLoaded = jest.spyOn(
+          TransactionActions,
+          'setTransactionsIsLoaded'
+        );
+
+        await dispatch(fetchBankAccountTransactions(fakeAccounts));
+
+        expect(setTransactions).toHaveBeenCalledWith({
+          transactions: fakeTransactions,
+        });
+
+        expect(setTransactionsIsLoaded).toHaveBeenCalledWith({
+          loaded: true,
+        });
+      });
     });
 
     describe('Given a bad response', () => {
-      it.todo('Throws a BadResponseError');
+      it('Throws a BadResponseError', async () => {
+        const dataFetchReturn = {
+          responsePromise: new Promise<any>((resolve, reject) => {
+            resolve({
+              ok: false,
+              json: () => ({
+                transactions: fakeTransactions,
+              }),
+              status: 500,
+            });
+          }),
+          cancel: () => null,
+        };
+
+        DataFetchMock.mockReturnValue(dataFetchReturn);
+
+        await dispatch(fetchBankAccountTransactions(fakeAccounts));
+
+        expect(BadResponseMock).toHaveBeenCalled();
+      });
     });
   });
   describe('addTransactionCategory', () => {
     describe('Given an ok response', () => {
-      it.todo('Dispatches categorizeTransaction and setTransactionsIsLoaded');
+      it('Dispatches categorizeTransaction and setTransactionsIsLoaded', async () => {
+        const dataFetchReturn = {
+          responsePromise: new Promise<any>((resolve, reject) => {
+            resolve({
+              ok: true,
+              json: () => ({
+                transactions: fakeTransactions,
+                message: 'SUCCESS',
+              }),
+            });
+          }),
+          cancel: () => null,
+        };
 
-      it.todo('Dispatches the thunk fetchAccountBalances');
+        DataFetchMock.mockReturnValue(dataFetchReturn);
+
+        const categorizeTransaction = jest.spyOn(
+          TransactionActions,
+          'categorizeTransaction'
+        );
+
+        const setTransactionsIsLoaded = jest.spyOn(
+          TransactionActions,
+          'setTransactionsIsLoaded'
+        );
+
+        await dispatch(addTransactionCategory(fakeAccounts[0], 1, 2, 'debit'));
+
+        expect(setTransactionsIsLoaded).toHaveBeenCalledWith({
+          loaded: false,
+        });
+
+        expect(categorizeTransaction).toHaveBeenCalledWith({
+          accountID: fakeAccounts[0].id,
+          categoryID: 2,
+          debitOrCredit: 'debit',
+          transactionID: 1,
+        });
+      });
     });
 
     describe('Given a bad response', () => {
-      it.todo('Throws a BadResponseError');
+      it('Throws a BadResponseError', async () => {
+        const dataFetchReturn = {
+          responsePromise: new Promise<any>((resolve, reject) => {
+            resolve({
+              ok: true,
+              json: () => ({
+                message: 'FAILURE',
+              }),
+              status: 500,
+            });
+          }),
+          cancel: () => null,
+        };
+
+        DataFetchMock.mockReturnValue(dataFetchReturn);
+
+        await dispatch(addTransactionCategory(fakeAccounts[0], 1, 2, 'debit'));
+
+        expect(BadResponseMock).toHaveBeenCalled();
+      });
     });
   });
   describe('addManyTransactionCategories', () => {
