889a8bf693b12216219479d191acdedc562c426a
Lukas <Lukas.Erekson@gmail.com>
2023-06-24 13:18:24 -0600

    Add custom MUI theme wrapper to whole app

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,54 @@ import AccountSettingsPage from './routes/AccountSettingsPage';
 import { ToastContainer } from 'react-toastify';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#32aaff',
+        main: '#008cff',
+        dark: '#0a79eb',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#fecd2b',
+        main: '#ffb607',
+        dark: '#ff7300',
+        contrastText: '#000',
+      },
+      warning: {
+        light: '#ee6e72',
+        main: '#de1724',
+        dark: '#d10924',
+        contrastText: '#fff',
+      },
+    },
+  });
+
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Navbar />
-        <div className='App'>
-          <Routes>
-            <Route path='/' element={<CategorizeTransactionsPage />} />
-            <Route
-              path='/categorize-transactions'
-              element={<CategorizeTransactionsPage />}
-            />
-            <Route path='/balance-sheet' element={<BalanceSheetPage />} />
-            <Route path='/expense-report' element={<ExpenseReportPage />} />
-            <Route path='/account/settings' element={<AccountSettingsPage />} />
-          </Routes>
-          <ToastContainer />
-        </div>
+        <ThemeProvider theme={theme}>
+          <Navbar />
+          <div className='App'>
+            <Routes>
+              <Route path='/' element={<CategorizeTransactionsPage />} />
+              <Route
+                path='/categorize-transactions'
+                element={<CategorizeTransactionsPage />}
+              />
+              <Route path='/balance-sheet' element={<BalanceSheetPage />} />
+              <Route path='/expense-report' element={<ExpenseReportPage />} />
+              <Route
+                path='/account/settings'
+                element={<AccountSettingsPage />}
+              />
+            </Routes>
+            <ToastContainer />
+          </div>
+        </ThemeProvider>
       </LocalizationProvider>
     </>
   );
